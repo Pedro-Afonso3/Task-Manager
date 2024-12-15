bb20f2d824d446a017ae9c6d2b98458cfec62297
Pedro-Afonso3 <paccj3@gmail.com>
2024-12-14 21:12:41 -0300

    feat: add Model Mapper

---
+++ src/main/java/com/agri_connect/Agri_connect/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package com.agri_connect.Agri_connect.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
