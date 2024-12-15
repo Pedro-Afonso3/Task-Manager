4d74d7c04e012690b207ed40ce7ffba9737b6df5
Pedro-Afonso3 <paccj3@gmail.com>
2024-12-14 21:38:17 -0300

    fix: fixing Repositories

--- src/main/java/com/agri_connect/Agri_connect/repositories/AgricultorRepository.java
+++ src/main/java/com/agri_connect/Agri_connect/repositories/AgricultorRepository.java
@@ -2,10 +2,12 @@ package com.agri_connect.Agri_connect.repositories;
 
 import com.agri_connect.Agri_connect.domain.agricultor.Agricultor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface AgricultorRepository extends JpaRepository<Agricultor, UUID> {
 
     Optional<Agricultor> findByName(String name);
--- src/main/java/com/agri_connect/Agri_connect/repositories/ProdutosRepository.java
+++ src/main/java/com/agri_connect/Agri_connect/repositories/ProdutosRepository.java
@@ -2,11 +2,16 @@ package com.agri_connect.Agri_connect.repositories;
 
 import com.agri_connect.Agri_connect.domain.produtos.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface ProdutosRepository extends JpaRepository<Produtos, UUID> {
 
-    Optional<Produtos> findByNomeProduto(String nomeProduto);
+    List<Produtos> findByNomeProduto(String nomeProduto);
+
+    Optional<Produtos> findById(UUID id);
 }
